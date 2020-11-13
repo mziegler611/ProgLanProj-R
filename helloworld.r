27b392f68d59c62138484fa6c1d26ab2f2325c74
mziegler611 <57671001+mziegler611@users.noreply.github.com>
2020-11-13 14:19:13 -0500

    Add files via upload

--- helloworld.r
+++ helloworld.r
@@ -2,11 +2,12 @@ print("Hello world")
 #read a csv file, and interpret the data
 #functions
     #simple ones
-        #print out the header(s)
-        #print out the data
+        #print out the header(s) -Lei
+        #print out the data -Morgan
     #less simple ones
-        #find the frequency of a result in a column
-        #find the average of a column if we are looking at numeric data
-        #find max or most occuring of a column  
-        #find min or least occuring of a column
-        #replace results in a column with another things (like for the dog data set: replace all instances of boxers with pugs)
+        #find the frequency of a result in a column -Morgan
+        #find function/ find location or percentage -Lei
+        #find max  -Lei
+        #find min -Lei
+        #find the average of a column -Morgan 
+       
