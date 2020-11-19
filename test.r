629529963859aec78d56f24810b5d5ac5c403aec
mziegler611 <57671001+mziegler611@users.noreply.github.com>
2020-11-19 14:24:34 -0500

    Added Test for Avg/Frequency

--- test.r
+++ test.r
@@ -1,12 +1,30 @@
 #import test library
 library(testthat)
 #import file
-source("/Users/leianna/Documents/321F20/ProgLanProj-R/functions.R", chdir = TRUE)
-#source("/Users/morganziegler/Desktop/ProgLanProj-R-main/functions.R", chdir = TRUE)
-
+source("/Users/leianna/Documents/321F20/ProgLanProj-R/main.R", chdir = TRUE)
+#source("/Users/morganziegler/Desktop/ProgLanProj-R-main/main.R")
 #Import table
 myData <- read_excel("/Users/leianna/Documents/321F20/final/catsvdogs.xlsx")
+#myData <- read_excel("/Users/morganziegler/Desktop/ProgLanProj-R-main/catsvdogs.xlsx")
+list_header<-names(myData)
 
-test_that("max", {
-  expect_equal(findMax(), list("Location", "Number of Households (in 1000)", "Percentage of households with pets", "Number of Pet Households (in 1000)", "Percentage of Dog Owners", "Dog Owning Households (1000s)", "Mean Number of Dogs per household", "Dog Population (in 1000)", "Percentage of Cat Owners", "Cat Owning Households", "Mean Number of Cats", "Cat Population"))
+
+print("Find Frequency Tests" , quote=FALSE)
+test_that("single number", {
+  expect_equal(findFrequency(myData, 3, 59.5), 2)
+  expect_equal(findFrequency(myData, 1, 'Nebraska'), 1)
+  expect_equal(findFrequency(myData, 5, -1), 0)
 })
+
+print("Find Average Tests", quote=FALSE)
+test_that("single number", {
+  expect_equal(findAvg(myData, 3), 56.859183)
+  expect_equal(findAvg(myData, 4), 1342.591836)
+  expect_equal(findAvg(myData, 5), 36.973469)
+})
+
+
+
+
+
+
